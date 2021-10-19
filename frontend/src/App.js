f91c7748aa4e10db00fc856684d5ad952f173b1f
Justin Wu <justinwu820@gmail.com>
2021-10-19 14:56:35 -0400

    Replaced anchor tag with Link tag

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,18 +3,21 @@ import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 const App = () => {
   return (
-    <>
+    <Router>
      <main>
       <Header />
         <Container>
-          <HomeScreen />  
+          <Route path="/" exact component={HomeScreen}/>
+          <Route path="/product/:id" exact component={ProductScreen} />
         </Container>
       </main> 
     <Footer />
-    </>
+    </Router>
   )
 }
 
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,20 +1,26 @@
 import React from 'react'
 import { Container,Navbar,Nav } from 'react-bootstrap'
-
+import { LinkContainer } from 'react-router-bootstrap'
 const Header = () => {
     return (
     <header>
 
         <Navbar bg="dark" variant="dark"expand="lg" collapseOnSelect>
                 <Container>
-                    <Navbar.Brand href="#">JNEcommerce</Navbar.Brand>
+                    <LinkContainer to="/">
+                        <Navbar.Brand>JNEcommerce</Navbar.Brand>
+                    </LinkContainer>
                     <Navbar.Toggle aria-controls="navbarScroll" />
                     <Navbar.Collapse id="navbarScroll">
                         <Nav
                         className="ms-auto my-2 my-lg-0"
                         >
-                        <Nav.Link href="#action1">Cart</Nav.Link>
-                        <Nav.Link href="#action2">Sign In</Nav.Link>
+                        <LinkContainer to="/cart">
+                            <Nav.Link href="#action1">Cart</Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to="/login">
+                            <Nav.Link href="#action2">Sign In</Nav.Link>
+                        </LinkContainer>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
